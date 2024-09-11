921ac5cb18d819a94dcfa5e318d23310cf2286cb
Maria Velandia <maria_velandia@epam.com>
2024-09-11 16:16:21 -0500

    Logic to be able to run the app. Add Application Context

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.facade.BookingFacade;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        BookingFacade bookingFacade = context.getBean(BookingFacade.class);
+
+        System.out.println("Application running!!!!");
     }
 }
